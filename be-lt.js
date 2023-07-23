b2a957af65a69e317e12680b54f3af1fa0641f9e
Top Cat <128065206+TopCat111@users.noreply.github.com>
2023-07-23 07:26:31 +0100

    Create be-lt.js
    BE lts

---
+++ be-lt.js
@@ -0,0 +1,24 @@
+function performLoadTests() {
+    /**
+     * This function performs back-end load tests.
+     * It simulates a high volume of requests to the server to measure its performance under load.
+     * 
+     * Parameters:
+     * None
+     * 
+     * Returns:
+     * None
+     */
+    
+    try {
+        // Perform load tests here
+        // ...
+        
+        console.log("Load tests completed successfully.");
+    } catch (error) {
+        console.error("An error occurred during load tests:", error);
+    }
+}
+
+// Example usage
+performLoadTests();
